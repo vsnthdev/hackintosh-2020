5cd0bacd7fef268612974b246026d8aff785b3ca
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-12-12 12:09:42 +0530

    create the hackintosh-2020 config.plist helper program

---
+++ README.md
@@ -0,0 +1,13 @@
+## Background Image Command
+```
+convert ./ingmar-MtVH3pwBTjs-unsplash.jpg -resize '1920x1080^' -gravity center -background white -extent 1920x1080 -filter Gaussian -blur 0x20 -fill black -colorize 40% ./background.png
+```
+[Download Image](https://unsplash.com/photos/MtVH3pwBTjs)
+
+## Kexts
+1. Apple ALC
+2. Lilu
+3. IntelMausiEthernet
+4. WhateverGreen
+5. VirtualSMC
+6. USBInjectAll
---
+++ src/compile.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import { program } from 'commander'
+import path from 'path'
+import fs from 'fs/promises'
+import yaml from 'js-yaml'
+import plist from 'plist'
+
+// verbose() will turn on or off verbosity
+// in the boot args
+const verbose = (sample, verbose) => {
+    if (verbose) sample.NVRAM.Add['7C436110-AB2A-4BBB-A880-FE41995C9F82']['boot-args'] = `${sample.NVRAM.Add['7C436110-AB2A-4BBB-A880-FE41995C9F82']['boot-args']} -v`
+}
+
+// smbios() will inject SMBIOS values
+// into config.sample.plist
+const smbios = async (sample, file) => {
+    const smbiosFile = path.resolve(file)
+
+    // check if smbios file exists
+    // and then read it
+    try {
+        await fs.stat(smbiosFile)
+    } catch {
+        console.log(`SMBIOS file at ${smbiosFile} doesn't exist.`)
+        process.exit(2)
+    }
+
+    const smbios = yaml.safeLoad(await fs.readFile(smbiosFile, { encoding: 'utf-8' }))
+
+    sample.PlatformInfo.Generic.SystemProductName = smbios.smbios
+    sample.PlatformInfo.Generic.MLB = smbios.board
+    sample.PlatformInfo.Generic.SystemSerialNumber = smbios.serial
+    sample.PlatformInfo.Generic.SystemUUID = smbios.uuid
+}
+
+const main = async () => {
+    program.name('hackintosh-2020')
+    .option('--smbios <file>', 'SMBIOS YAML file')
+    .option('--out <file>', 'Compiled config.plist for OpenCore')
+    .option('-v, --verbose', 'Enable or disable verbose mode.', false)
+    .parse(process.argv)
+
+    // remove junk from Commander's program
+    const remove = [ 'commands', 'options', 'parent', 'rawArgs', 'program', 'Command', 'Option', 'CommanderError', 'args']
+    let args = {}
+    Object.keys(program).filter(key => key.startsWith('_') == false).filter(key => remove.includes(key) == false).forEach(key => args[key] = program[key])
+
+    // handle initial command
+    if (!args.smbios || !args.out) {
+        program.outputHelp()
+        process.exit(0)
+    }
+    
+    // read the plist file
+    const sample = plist.parse(await fs.readFile('data/config.sample.plist', { encoding: 'utf-8' }))
+    
+    // inject SMBIOS values
+    await smbios(sample, args.smbios)
+    await verbose(sample, args.verbose)
+
+    // write back the file
+    // white converting it back into a plist file
+    await fs.writeFile(path.resolve(args.out), plist.build(sample))
+
+    console.log('✅ done!')
+}
+
+// start the cli
+main()
